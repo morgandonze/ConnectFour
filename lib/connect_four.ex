1a42e3a17bd0b2ed092f8f239231b2d47c693ba7
Morgan Laco <morgan@codeschool.com>
2016-06-28 19:12:51 -0400

    Just getting started

---
+++ lib/connect_four.ex
@@ -0,0 +1,51 @@
+defmodule ConnectFour do
+  def play do
+    state = %{
+      turn: 1,
+      board: ConnectFour.Board.new,
+    }
+    do_turn(state)
+  end
+  
+  def do_turn(state) do
+    display_board(state[:board])
+    
+    case get_move(state[:turn]) do
+      {:ok, move} ->
+        # do stuff
+      {:error} ->
+        # do error stuff
+    end
+    
+  end
+  
+  def get_move(turn) do
+    IO.gets(:stdio, "[#{translate_player(turn)}] Enter move: ")
+    |> String.trim
+    |> validate_move
+  end
+  
+  def validate_move(move) do
+    if String.match?(move, ~r/\d/) do
+      {:ok, move}    
+    else
+      {:error}
+    end
+  end
+  
+  def display_board(board) do
+    IO.inspect board
+  end
+  
+  def translate_player(turn) do
+    case turn do
+      1 ->
+        "O"
+      -1 ->
+        "@"
+      _ ->
+        ""  
+    end
+  end
+  
+end
