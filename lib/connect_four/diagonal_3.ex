abe22fec292a6414f3ac3666659c21acae12fe4c
Morgan Laco <morgan@codeschool.com>
2016-06-30 22:22:50 -0400

    Fix diagonal checker

--- lib/connect_four/diagonal_3.ex
+++ lib/connect_four/diagonal_3.ex
@@ -1,9 +1,4 @@
 defmodule ConnectFour.Diagonal3 do
-  def add_item(item, item_rem, [], matrix_rem, range) do
-    [mh|mt] = matrix_rem
-    add_item(item, item_rem, mh, mt, range)
-  end
-  
   def add_item(item, [], acc, [], _) do
     pos = Enum.count(acc)
     List.insert_at(acc, pos, [item])
@@ -20,15 +15,17 @@ defmodule ConnectFour.Diagonal3 do
     cond do
       Enum.count(matrix_rem) >= Enum.min(range) &&
       Enum.count(matrix_rem) <= Enum.max(range) ->
-        [ih | it] = item_rem
+        [ih | it] = Enum.reverse(item_rem)
         pos = Enum.count(acc)
         mhpos = Enum.count(mh)
-        new_row = List.insert_at(mh, mhpos, item)
+        new_row = List.insert_at(mh, mhpos, item) 
         new_acc = List.insert_at(acc, pos, new_row)
         
         add_item(ih, it, new_acc, mt, range)  
       true ->
-        add_item(item, item_rem, [acc, mh], mt, range)
+        pos = Enum.count(acc)
+        new_acc = List.insert_at(acc, pos, mh)
+        add_item(item, item_rem, new_acc, mt, range)
     end
   end  
 end
--- test/connect_four/diagonal_3_test.exs
+++ test/connect_four/diagonal_3_test.exs
@@ -3,27 +3,25 @@ defmodule ConnectFour.Diagonal3Test do
   doctest ConnectFour.Diagonal3
   
   test "#add_item" do
-    result = [ [ 7], [4,8],[1,5],[2]]
+    result = [[7],[4,8],[1,5],[2]]
+    range = [1,2]
     
     item = 2
     item_rem = []
     acc = [[7],[4,8],[1,5]]
     matrix_rem = []
-    range = [1,2]
     assert ConnectFour.Diagonal3.add_item(item, item_rem, acc, matrix_rem, range) == result
     
     item = 5
     item_rem = [2]
-    acc = [[7,],[4,8]]
+    acc = [[7],[4,8]]
     matrix_rem = [[1]]
-    range = [1,2]
     assert ConnectFour.Diagonal3.add_item(item, item_rem, acc, matrix_rem, range) == result
     
     item = 8
-    item_rem = [5,2]
+    item_rem = [2,5]
     acc = []
     matrix_rem = [[7],[4],[1]]
-    range = [1,2]
-    assert ConnectFour.Diagonal3.add_item(item, item_rem, acc, matrix_rem, range) == result
+    assert ConnectFour.Diagonal3.add_item(item, item_rem, acc, matrix_rem, range) == [[7],[4,8],[1,5],[2]]
   end
 end
